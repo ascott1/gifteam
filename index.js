71b9b3c9923071548b89a3facc4ef736c6feaea5
Adam Scott <adam.scott@cfpb.gov>
2016-08-03 11:07:23 -0400

    cleanup and es6ify

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+const PUBLIC = path.resolve(__dirname, 'public');
 
-io.on('connection', function(socket){
-  // fire event when a user connects
-  socket.on('user gif', function(msg){
-    io.emit('user gif', msg);
-  });
-  // fire event when a user disconnects
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+const server = express()
+  .use(express.static(PUBLIC) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  // the gif event
+  socket.on('giffed', () => io.emit('giffed'));
+  // event when a user disconnects
+  socket.on('disconnect', () => console.log('user disconnected'));
+
 });
